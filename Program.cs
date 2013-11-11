11e660dfe34552fa71b1c3ad86ca83c8d3a893d9
HanSolo <HanSolo@forum.kncminer.com>
2013-11-11 23:07:44 +1100

    Temp sensor working

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace TempControlDuino
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+            while (true)
+            {
+                AnalogInput pot = new AnalogInput(AnalogChannels.ANALOG_PIN_A0);
+
+                int potValue = 0;
+
+                while (true)
+                {
+                    //read the value of the potentiometer
+                    potValue = (int)pot.ReadRaw();
+                    Debug.Print("ReadRaw analog value: " + pot.ReadRaw());
+
+                    Thread.Sleep(1000); 
+                    
+                } 
+            }
+
+        }
+
+    }
+}
